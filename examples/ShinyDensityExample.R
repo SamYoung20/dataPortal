9b3d6481f87369ba894f8a4620f10118c048ec3a
212628167 <rupayan.neogy@ge.com>
2017-06-19 15:55:21 -0400

    added checkboxes to zone example, interactive slightly

--- examples/ShinyDensityExample.R
+++ examples/ShinyDensityExample.R
@@ -2,8 +2,8 @@ library(shiny)
 library(leaflet)
 
 # Police zones
-waterRiseLink <- "http://bostonopendata-boston.opendata.arcgis.com/datasets/bfe3e93c27004a69921b629b92cd1f9f_0.geojson"
-waterRiseData <- readLines(policeZoneLink) %>% paste(collapse = "\n")
+waterRiseLink <- "http://bostonopendata-boston.opendata.arcgis.com/datasets/eebd3daed05a45678894db30d9bf0cfb_0.geojson"
+waterRiseData <- readLines(waterRiseLink) %>% paste(collapse = "\n")
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
@@ -14,10 +14,10 @@ server <- function(input, output, session) {
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
+      addProviderTiles(providers$CartoDB.Positron,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addGeoJSON(waterRiseData, weight = 1, color = "red", fill = TRUE) %>%
+      addGeoJSON(waterRiseData, weight = 3, color = "red", fill = FALSE) %>%
       fitBounds(-71.5, 42, -70.5, 42.5)
   })
 }
--- examples/ShinyZoneExample.R
+++ examples/ShinyZoneExample.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(leaflet)
-library(RColorBrewer)
 
 # Police zones
 policeZoneLink <- "http://bostonopendata-boston.opendata.arcgis.com/datasets/9a3a8c427add450eaf45a470245680fc_5.geojson"
@@ -11,28 +10,38 @@ policeStationData <- readLines(policeStationLink) %>% paste(collapse = "\n")
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("mymap", width = "100%", height = "100%"),
+  leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
                 sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
                             value = range(quakes$mag), step = 0.1
                 ),
-                selectInput("colors", "Color Scheme",
-                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                ),
-                checkboxInput("legend", "Show legend", TRUE)
+                selectInput("datasets", "Data Set", c("Police Data")),
+                checkboxInput("stationCheckbox", "Police Stations", TRUE),
+                checkboxInput("zoneCheckbox", "Police Zones", TRUE)
   )
 )
 
 server <- function(input, output, session) {
   
-  output$mymap <- renderLeaflet({
+  output$map <- renderLeaflet({
     leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addGeoJSON(policeStationData) %>%
-      addGeoJSON(policeZoneData, weight = 3, color = "red", fill = FALSE) %>%
+      # addLayersControl(
+      #   overlayGroups = c("Police Stations", "Police Zones"),
+      #   options = layersControlOptions(collapsed = FALSE)
+      # ) %>%
       fitBounds(-71.5, 42, -70.5, 42.5)
+  });
+  
+  observe({
+    leafletProxy("map") %>%
+      clearShapes() %>%
+      addGeoJSON(policeStationData, layerId = "policeStations", 
+                 opacity = ifelse(input$stationCheckbox, 1, 0)) %>%
+      addGeoJSON(policeZoneData, layerId = "policeZones", weight = 2, color = "blue",
+                 fill = TRUE, opacity = ifelse(input$zoneCheckbox, 1, 0))
   })
 }
 
