05ceaf66679b97bb3c0ee91d70fd020b329c4c5f
212628167 <rupayan.neogy@ge.com>
2017-06-21 14:45:19 -0400

    Veronica's app relocated and reorganized

--- VeronicaMap/Initial Map Script.R
+++ VeronicaMap/Initial Map Script.R
@@ -2,15 +2,15 @@ library(dplyr)
 library(shiny)
 library(leaflet)
 library(htmltools)
-wins <- read.csv("/Users/veronica/downloads/wins.csv")
-greenLeafIcon <- makeIcon(iconUrl = "/Users/veronica/downloads/Untitled-1.svg", iconWidth = 50, iconHeight = 50, iconAnchorX = 0, iconAnchorY = 0)
+wins <- read.csv("Wins1.csv")
+greenLeafIcon <- makeIcon(iconUrl = "Map_marker.png", iconWidth = 40, iconHeight = 60, iconAnchorX = 0, iconAnchorY = 0)
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%")) 
 server <- function(input, output) {
   output$map <- renderLeaflet({
-    m <- leaflet(wins) %>% addTiles('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
-                                    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') 
+    m <- leaflet(wins) %>% addProviderTiles(providers$CartoDB.Positron,
+                                            options = providerTileOptions(noWrap = TRUE)) 
     m %>% setView(-72.690940, 41.651426, zoom = 5)
     m %>% addCircles(~lng, ~lat, popup=wins$type, weight = 7, radius=70, 
                      color="#0017bb", stroke = TRUE, fillOpacity = .8) 
